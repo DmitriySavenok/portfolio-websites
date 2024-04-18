67c41325bd166ee7ac6e1319dd854a296ab731ee
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2024-04-18 15:15:56 +0300

    edit accordion project

--- portfolio/accordionRepairWorkshop/js/script.js
+++
@@ -1,5 +0,0 @@
-AOS.init({
-  disable: 'mobile',
-  once: true, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-});
